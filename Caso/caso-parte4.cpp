ada890df13857016e4d106ad71417915f2802eb0
Daniel-tech-sys <danielrafael9729@gmail.com>
2025-03-09 14:05:15 -0500

    Final C++

--- Caso/caso-parte4.cpp
+++ Caso/caso-parte4.cpp
@@ -11,26 +11,30 @@ int main()
     int talla, costo;
 
     // Ingreso de datos
-    cout << "Desea registrar un Zapato s/n: " << endl;
+    cout << "Desea registrar un Zapato s/n: ";
     cin >> registro;
 
     // Ciclo While
     while (registro == 's') {
-        cout << "Digite la referencia: " << endl;
+        cout << "Digite la referencia: ";
         cin >> referencia;
-        cout << "Digite la descripcion: " << endl;
+        cout << "Digite la descripcion: ";
         cin.ignore();
         getline(cin, descripcion);
-        cout << "Digite la talla: " << endl;
+        cout << "Digite la talla: ";
         cin >> talla;
-        cout << "Costo: " << endl;
+        cout << "Costo: ";
         cin >> costo;
+        cout << " " << endl;
 
-        cout << "Desea registrar OTRO Zapato s/n: " << endl;
+        cout << "Desea registrar OTRO Zapato s/n: ";
         cin >> registro;
+        cout << " " << endl;
     }
 
     cout << "FIN DE LA APLICACION" << endl;
+
+    cout << "Daniel Alba" << endl;
 
     system("pause");
 
